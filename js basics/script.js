4f627a22a84794b3596121b5260d655191947510
Avika <avika.jainn18@gmail.com>
2026-02-24 10:33:23 +0530

    js basics

---
+++ js basics/script.js
@@ -0,0 +1,22 @@
+console.log("Hello world");
+document.write("Helloo World");
+
+var num=10;
+console.log(num);
+console.log(typeof(num))
+num =false; 
+console.log(num);
+console.log(typeof(num))
+num =null;
+console.log(num);
+console.log(typeof(num))
+num =undefined;
+console.log(num);
+console.log(typeof(num))
+var sayHeloo= function(){
+    alert("Helloowoelrdds ")
+}
+sayHeloo;
+ var array=[1,2,3];
+ var animals= newArrat("cat","dog")
+
