b7f9130967358534768aef77973fb0463aae0868
Avika <avika.jainn18@gmail.com>
2026-02-27 12:06:03 +0530

    conditonal statements

---
+++ conditions/if-else.js
@@ -0,0 +1,7 @@
+let age=20;
+if (age>=18){
+    console.log("Youre eleigible to vote");
+}
+else{
+    consoole.log("Not old enough");
+}
---
+++ conditions/switch.js
@@ -0,0 +1,16 @@
+var grade='A';
+document.write("Entering switch block <br/>");
+switch(grade){
+    case 'A': document.write("Good Job <br/>");
+    break;
+    case 'B': document.write("Pretty Job <br/>");
+    break;
+    case 'C': document.write("Not so good <br/>");
+    break;
+    case 'D': document.write("Passed <br/>");
+    break;
+    case 'F': document.write("Good Job <br/>");
+    break;
+    default: document.write("Unknown Grade <br/>")
+}
+document.write("Exiting Switch block");
