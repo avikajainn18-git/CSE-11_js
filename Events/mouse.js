4afc4b4c7a4079780289ca4876f2474a3923c976
Avika <avika.jainn18@gmail.com>
2026-03-17 09:47:45 +0530

    mouse events

---
+++ Events/mouse.js
@@ -0,0 +1,18 @@
+function onClick(){
+    console.log("clicked");
+}
+function onMouseOver(){
+    console.log(" On Mouse Over")
+}
+function onMouseOut(){
+    console.log(" On Mouse Out")
+}
+function onMouseUp(){
+    console.log(" On Mouse Up")
+}
+function onMouseDown(){
+    console.log(" On Mouse Down")
+}
+function onMouseMove(){
+    console.log(" On Mouse Move")
+}
