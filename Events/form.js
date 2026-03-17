97abbe3006e424a5d1313fe0430cfe69d41976bb
Avika <avika.jainn18@gmail.com>
2026-03-17 09:55:34 +0530

    form events

---
+++ Events/form.js
@@ -0,0 +1,19 @@
+function onSubmit(){
+    console.log("Form Submitted");
+}
+
+function onReset(){
+    console.log("Form Reset");
+}
+
+function onFocus(){
+    console.log("Input Focus");
+}
+
+function onBlur(){
+    console.log("Input Blur");
+}
+
+function onChange(){
+    console.log("Value Changed");
+}
